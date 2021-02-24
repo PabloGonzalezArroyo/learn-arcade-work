9e4a6ff3fa0618606903ebf174e94551edc1954c
Pablo González <pablo.gonzaleza@edu.uah.es>
2021-02-24 20:18:00 +0100

    My first program on how to differently use print

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,25 @@
-Hi
+# Intro, first line
+print("Dear diary:")
+
+# First paragrpah
+print("\nToday I figured out how to use git. I'm not going to lie and say \"Yeah, it was easy\" because it wasn't.")
+print("Sure, it's just code but it isn't as easy as Python or even Java are, due to the abstract nature of it.")
+print("""For example, just to get git working where I wanted I had to type:
+            >cd Desktop \t\t\t (entering the Desktop folder)
+            >cd TecVid \t\t\t\t (then my own folder)
+            >git clone whatever \t (to clone what was on GitHub to my laptop)
+            >cd learn-arcade-work \t (to then enter on the new cloned folder)
+Coming from Python, where \"print\" is the literal meaning of the word... It may seem a little bit weird.""")
+
+# Second paragraph
+print("\nBut don't get me wrong! Seeing what was in C:\\Users\\myuser\\Desktop\\TecVid just suddenly appear on GitHub")
+print("was actually really impressive and \"\\COOL\\\"!")
+print("""We just had to use the following command:
+            >git status \t\t\t (just for checking purposes)
+            >git add * \t\t\t\t (to simplify work)
+            >git commit -m "Comment" (to register the change and a description stating what did I do)
+            >git push \t\t\t\t (to actually upload the project to the server)""")
+print("\nAnd that was it. Pretty fun and interesting, right? Well, now I have to commit and push this file. Bye!")
+
+# Signature
+print("\n\\Pablo.")
