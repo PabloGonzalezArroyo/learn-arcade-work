5531f957fc43f5c8a7c3dc6c96d54c6be8ad6f60
Pablo González <pablo.gonzaleza@edu.uah.es>
2021-02-24 19:29:22 +0100

    Finished Testing work in lab 1

--- Testing/test.py
+++ Testing/test.py
@@ -1,2 +1,37 @@
-print("It was a dark and stormy night.")
-print("Suddenly a shot rang out!")
+#1
+print("Hi! This is a double quote mark: \" We use it for printing.")
+
+#2
+print("Audrey Hepburn once said \"Nothing is impossible. The word itself says 'I'm Possible!'.\"")
+
+#3
+print("Audrey Hepburn once said \"Nothing is impossible. The word itself says 'I'm Possible!'.\"")
+print("The file is stored in C:\\new folder")
+
+#4
+print("This \n is \n my \n sample.")
+
+#5
+print("""You can print
+on multiple
+lines using
+triple
+quotes.""")
+print("""You can print
+        on multiple
+        lines using
+        triple
+        quotes.""")
+
+#Review questions
+#1: NameError
+#2: SyntaxError
+#3: Parameter
+#4: \"
+#5: \n
+#6:
+print("Escape codes: \\\\")
+#7: Tripel Quotes """
+#8:
+print("Hello")
+#9: PEP-8
